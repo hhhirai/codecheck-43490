05fa0c8850ab6b4efc9ddd734adcbe4b71c81524
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-18 23:01:37 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,11 +1,6 @@
 package codecheck;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class App {
 	public static void main(String[] args) {
@@ -13,33 +8,34 @@ public class App {
 		URL url;
 		for (int i = 0, l = args.length; i < l; i++) {
 			String input = String.format("argv[%s]: %s", i, args[i]);
-		try {
-			url = new URL("http://challenge-server.code-check.io/api/hash?q=" + input);
-
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(),
-                                                                   StandardCharsets.UTF_8);
-                    BufferedReader reader = new BufferedReader(isr)) {
-                    String line = "";
-                    while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                }
-            }
-
-         } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
+			System.out.println(input);
+//		try {
+//			url = new URL("http://challenge-server.code-check.io/api/hash?q=" + input);
+//
+//        HttpURLConnection connection = null;
+//        try {
+//            connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//
+//            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+//                try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(),
+//                                                                   StandardCharsets.UTF_8);
+//                    BufferedReader reader = new BufferedReader(isr)) {
+//                    String line = "";
+//                    while ((line = reader.readLine()) != null) {
+//                        System.out.println(line);
+//                    }
+//                }
+//            }
+//
+//         } finally {
+//            if (connection != null) {
+//                connection.disconnect();
+//            }
+//        }
+//        } catch (IOException e) {
+//        e.printStackTrace();
+//        }
 	}
 	}
 
