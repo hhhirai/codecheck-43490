92f365d7dce3ee081b3e9a69b886aa5f5386b225
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-18 21:42:35 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,13 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.maven.plugins;
+import org.codehaus.mojo;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
