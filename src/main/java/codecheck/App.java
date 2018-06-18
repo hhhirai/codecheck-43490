e2c4a5cf22eaef8ed1cba52a6aa6f011e806a60f
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-18 23:20:53 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,12 +1,12 @@
 package codecheck;
 
-import java.net.URL;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+
 
 
 public class App {
@@ -18,19 +18,21 @@ public class App {
 
 		try {
 			url = new URL("http://challenge-server.code-check.io/api/hash?q=" + input);
+			StringBuilder builder = new StringBuilder();
 
-        HttpURLConnection connection = null;
+			HttpURLConnection connection = null;
         try {
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
                 try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(),
                                                                    StandardCharsets.UTF_8);
                     BufferedReader reader = new BufferedReader(isr)) {
                     String line = "";
                     while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
+                    	builder.append(line);
                     }
                 }
             }
@@ -43,6 +45,17 @@ public class App {
         } catch (IOException e) {
         e.printStackTrace();
         }
+
+		try {
+			JSONArray jsonArray = new JSONArray(builder.toString());
+			System.out.println("Number of entries " + jsonArray.length());
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonObject = jsonArray.getJSONObject(i);
+				System.out.println("投稿日："+jsonObject.getString("hash"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	}
 
