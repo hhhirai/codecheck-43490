9b1649cca46479d211dffdfddccfa20278afb06a
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-18 23:22:40 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -6,7 +6,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class App {
