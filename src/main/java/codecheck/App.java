df7093ed3052879db5ac76ab55069cc47409b42e
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-18 21:56:24 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,59 +1,43 @@
 package codecheck;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import org.apache.maven.plugins;
-import org.codehaus.mojo;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+
 
 public class App {
 	public static void main(String[] args) {
 
-		String uri = "http://challenge-server.code-check.io/api/hash?q=";
+		URL url = new URL("http://challenge-server.code-check.io/api/hash?p=fizz");
 
-		String input = "fizz";
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-		StringBuilder builder = new StringBuilder();
-		HttpClient client = new DefaultHttpClient();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(),
+                                                                   StandardCharsets.UTF_8);
+                     BufferedReader reader = new BufferedReader(isr)) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                }
+            }
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 
-		HttpGet httpGet = new HttpGet(uri + input);
-		try {
-			HttpResponse response = client.execute(httpGet);
-			//返却されたHTTPレスポンスの中のステータスコードを調べる
-			// -> statusCodeが200だったらページが存在。404だったらNot found（ページが存在しない）。500はInternal server error。
-			int statusCode = response.getStatusLine().getStatusCode();
-			if (statusCode == 200) {
-				HttpEntity entity = response.getEntity();
-				InputStream content = entity.getContent();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-				String line;
-				while ((line = reader.readLine()) != null) {
-					builder.append(line);
-				}
-			} else {
-				System.out.println("Failed to download file");
-			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// 文字列をJSONオブジェクトに変換する
-		try {
-			JSONArray jsonArray = new JSONArray(builder.toString());
-			System.out.println("Number of entries " + jsonArray.length());
-			for (int i = 0; i < jsonArray.length(); i++) {
-				JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                System.out.println(i);
-				System.out.println("入力値："+jsonObject.getString("q"));
-				System.out.println("ハッシュ値："+jsonObject.getString("hash"));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
